9ac4962c10b4bd520839d2b4e30806db9b9d8f02
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-06-02 19:40:44 -0300

    Implementa a listagem dos items da api (back-end)

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import knex from './database/connection'
 
 const routes = express.Router()
 
@@ -6,8 +7,17 @@ const routes = express.Router()
 1° parâmetro: rota
 2° parâmetro: Função que será executada assim que o usuário acessar essa rota. O request é utilizado para obter dados  da requisição que está acontecendo na aplicação. O response serve para devolver uma resposta para p browser ou para qualquer aplicação que esteja consumindo essa rota.
 */
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Hello word' })
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*')
+
+    const serializedItems = items.map(item => {
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`
+        }
+    })
+
+    return response.json(serializedItems)
 })
 
 export default routes
