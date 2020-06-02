43f54135b3a7acbc1e2fc861665237bda612bdbb
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-06-02 14:36:13 -0300

    Cria a conexão com o banco de dados

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,5 +1,12 @@
 import Knex from 'knex'
+import path from 'path'
 
 const connection = Knex({
     client: 'sqlite3',
+    connection: {
+        // Armazena o arquivo do banco de dados
+        filename: path.resolve(__dirname, 'database.sqlite ')
+    }
 })
+
+export default connection
